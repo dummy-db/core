414e327420c3c01c830bed85caa61aa206947924
Casper Tollefsen <casper.tollefsen@telenor.no>
2024-08-24 14:31:14 +0200

    Replace error with warning for unused variables in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,5 +8,8 @@ export default tseslint.config(
   ...tseslint.configs.recommended,
   {
     ignores: ['dist', 'node_modules'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
   },
 )
